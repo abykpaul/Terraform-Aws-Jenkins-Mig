abbc01273fa017d03c351cd58195cb13128a7613
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-06-18 21:31:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-jenkins-bucket"   # 🔁 Change this to your bucket name
-    key            = "jenkins/terraform.tfstate"
+    bucket         = "my-terraform-jenkins-bucket-aby01"   # 🔁 Change this to your bucket name
+    key            = "terraform.tfstate"
     region         = "us-east-1"
   }
 }
